b8eb46e132c1542aff3d11130895656ef82dbefb
Mathew Rolfe <mathew.rolfe@mn.catholic.edu.au>
2024-10-22 15:04:28 +1100

    first commit

---
+++ AllSaintsDay/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AllsaintsdayConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'AllSaintsDay'
---
+++ AllSaintsDay/views.py
@@ -0,0 +1,9 @@
+import datetime
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    now = datetime.datetime.now()
+    return render(request, "AllSaintsDay/index.html", {
+        "AllSaintsDay": now.month ==10 and now.day == 2
+    })
